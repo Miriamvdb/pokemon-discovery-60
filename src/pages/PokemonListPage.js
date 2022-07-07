30433b0d55e07968c897ee62958f7300a9cfc1f2
Miriamvdb <miriamvdb@live.nl>
2022-07-07 16:45:01 +0200

    Changed file/component name

---
+++ src/pages/PokemonListPage.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import PokemonCard from "../components/PokemonCard";
+
+const PokemonListPage = () => {
+  const [pokemonList, setPokemonList] = useState(null);
+
+  useEffect(() => {
+    // console.log("Hello???");
+    const getPokemons = async () => {
+      const response = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=151"
+      );
+      setPokemonList(response.data.results);
+      // console.log("Pokemon?", response.data);
+    };
+    getPokemons();
+  }, []);
+
+  return (
+    <div>
+      <h3>Pokemon</h3>
+      {pokemonList
+        ? pokemonList.map((pokemonObj, index) => {
+            return <PokemonCard key={index} name={pokemonObj.name} />;
+          })
+        : "Loading.."}
+    </div>
+  );
+};
+
+export default PokemonListPage;
